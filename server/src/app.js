d741d2e0f43a9c13b3dbdc8a8f803b46d17929ea
Filip Sarovic <bombadilforn@gmail.com>
2026-05-08 16:17:27 +0200

    Revert "register user"

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
-// routes
 import uploadRoutes from "./routes/uploadRoutes.js";
 import documentRoutes from "./routes/documentRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();    
 const app = express();
@@ -18,7 +16,6 @@ app.use(express.json());
 
 app.use("/api/upload", uploadRoutes);
 app.use("/api/documents", documentRoutes);
-app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("API is running..");
