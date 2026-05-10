3652dd273be853118a114719fbe7d65c3d1b5c3c
lastoned <filipsarovic428@gmail.com>
2026-05-10 15:52:55 +0200

    created auth middleware with jwt token

---
+++ server/src/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+
+export const protect = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({
+        message: "No token provided",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+
+    req.user = decoded;
+
+    next();
+
+  } catch (error) {
+    console.error("AUTH ERROR:", error);
+
+    res.status(401).json({
+      message: "Invalid token",
+    });
+  }
+};
